71b591ae0052a1eb9e78cdaec55d1dfc07b4000a
shah <shahrokh.r1379@gmail.com>
2025-08-15 19:18:15 +0330

    vuln_app

---
+++ src/vuln_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    password = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.username
